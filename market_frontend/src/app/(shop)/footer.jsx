16d392d1e3d50cb54f683aa3cd85091447a51798
itaipemper <itaipem@post.bgu.ac.il>
2024-05-27 21:17:43 +0300

    init frontend

---
+++ market_frontend/src/app/(shop)/footer.jsx
@@ -0,0 +1,35 @@
+
+export default function Footer() {
+  return (
+
+
+    <footer class="bg-gray-800 text-white text-center py-1">
+        <div class="w-full max-w-screen-xl mx-auto p-2 md:py-1">
+            <div class="sm:flex sm:items-center sm:justify-between">
+                <a href="https://flowbite.com/" class="flex items-center mb-4 sm:mb-0 space-x-3 rtl:space-x-reverse">
+                    <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" />
+                    <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">SababaSales</span>
+                </a>
+                <ul class="flex flex-wrap items-center mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+                    <li>
+                        <a href="#" class="hover:underline me-4 md:me-6">About</a>
+                    </li>
+                    <li>
+                        <a href="#" class="hover:underline me-4 md:me-6">Privacy Policy</a>
+                    </li>
+                    <li>
+                        <a href="#" class="hover:underline me-4 md:me-6">Licensing</a>
+                    </li>
+                    <li>
+                        <a href="#" class="hover:underline">Contact</a>
+                    </li>
+                </ul>
+            </div>
+            <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-2" />
+            <span class="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 <a href="https://SababaSales.com/" class="hover:underline">SababasSales™</a>. All Rights Reserved.</span>
+        </div>
+    </footer>
+
+
+  );
+}
---
+++ market_frontend/src/app/(shop)/layout.jsx
@@ -0,0 +1,43 @@
+"use client"
+import React from 'react';
+import NavBar from './navbar'; // Assuming Example component is in a separate file
+import Footer from './footer'; // Assuming Example component is in a separate file
+import Cart from './cart'; // Assuming Example component is in a separate file
+
+const UserContext = React.createContext({
+  loggedIn: false,
+  userName: null,
+  id: null,
+});
+
+const Layout = ({ children }) => {
+  const [user, setUser] = React.useState({
+    loggedIn: false,
+    userName: null,
+    id: null,
+  });
+  const [cart, setCart] = React.useState(true);
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+    <div className="flex flex-col h-screen">
+      {/* Navigation Bar */}
+      <NavBar setCart = {setCart} />
+      <Cart isOpen={cart} setCart = {setCart}/>
+
+      {/* Main Content */}
+      <main className="flex-1 overflow-y-auto bg-gray-100">
+        {children}
+      </main>
+
+      {/* Footer (if needed) */}
+      {/* <footer className="bg-gray-800 text-white text-center py-4"> */}
+        {/* {/* Your footer content here */}
+      {/* </footer>  */}
+      <Footer />
+    </div>
+    </UserContext.Provider>
+  );
+};
+
+export default Layout;
+export { UserContext };
