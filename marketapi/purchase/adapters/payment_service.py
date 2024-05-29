1095042222b7bdd726f5fba5c13519d70e13d17d
royweiss <weissroy@post.bgu.ac.il>
2024-05-29 12:11:23 +0300

    make purchase fixes. still need to add functions to user facade

--- marketapi/purchase/adapters/payment_service.py
+++ marketapi/purchase/adapters/payment_service.py
@@ -4,4 +4,4 @@ from purchase.models import PaymentMethod
 
 class AbstractPaymentService(PaymentServiceInterface):
     def process_payment(self, payment_method, flag: PaymentMethod) -> dict:
-        return not flag
+        return {"result": flag}
