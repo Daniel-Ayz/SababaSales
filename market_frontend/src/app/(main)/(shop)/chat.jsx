8b90ced5269bb5cd963316fd9e20c9c4d9a8307a
Daniel-Ayz <ayzendan@post.bgu.ac.il>
2024-06-26 16:36:11 +0300

    Beautiful Notifications with React Toastify 🦄

--- market_frontend/src/app/(main)/(shop)/chat.jsx
+++ market_frontend/src/app/(main)/(shop)/chat.jsx
@@ -1,20 +1,39 @@
 import React, {useState, useEffect, useRef, useContext} from 'react';
 import {UserContext} from "@/app/(main)/layout";
 
+import {ToastContainer, toast, Bounce} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 function ChatApp() {
     const [message, setMessage] = useState('');
     const [messages, setMessages] = useState([]);
     const chatSocket = useRef(null);
-    const {user,setUser} = useContext(UserContext);
+    const {user} = useContext(UserContext);
+
+    const notify = (msg) => toast('🦄' + msg, {
+                                                    position: "top-right",
+                                                    autoClose: 10000,
+                                                    hideProgressBar: false,
+                                                    closeOnClick: false,
+                                                    pauseOnHover: true,
+                                                    draggable: true,
+                                                    progress: undefined,
+                                                    theme: "dark",
+                                                    transition: Bounce,
+                                                    });
 
     useEffect(() => {
+        console.log("ChatApp useEffect")
+        /* Hard coded URL to localhost for development - websocket connection */
         const url = `ws://localhost:8000/ws/socket-server/`;
         chatSocket.current = new WebSocket(url);
 
         chatSocket.current.onmessage = function(e) {
             const data = JSON.parse(e.data);
             if (data.type === 'chat') {
+                console.log("message: " + data.message);
                 setMessages(prevMessages => [...prevMessages, data.message]);
+                notify(data.message);
             }
         };
 
@@ -35,19 +54,24 @@ function ChatApp() {
     };
 
     return (
+        // <div>
+        //     {user.loggedIn ? <h1>Welcome {user.userName}!</h1> : <h1>Welcome Guest!</h1>}
+        //     <h1>Lets chat!</h1>
+        //     <button onClick={notify}>Notify</button>
+        //     <ToastContainer/>
+        //     <form id="form" onSubmit={handleSubmit}>
+        //         <input type="text" name="message" value={message} onChange={(e) => setMessage(e.target.value)} />
+        //     </form>
+        //     <div id="messages">
+        //         {messages.map((msg, index) => (
+        //             <div key={index}>
+        //                 <p>{msg}</p>
+        //             </div>
+        //         ))}
+        //     </div>
+        // </div>
         <div>
-            {user.loggedIn ? <h1>Welcome {user.userName}!</h1> : <h1>Welcome Guest!</h1>}
-            <h1>Lets chat!</h1>
-            <form id="form" onSubmit={handleSubmit}>
-                <input type="text" name="message" value={message} onChange={(e) => setMessage(e.target.value)} />
-            </form>
-            <div id="messages">
-                {messages.map((msg, index) => (
-                    <div key={index}>
-                        <p>{msg}</p>
-                    </div>
-                ))}
-            </div>
+            <ToastContainer/>
         </div>
     );
 }
--- marketapi/users/consumers.py
+++ marketapi/users/consumers.py
@@ -36,6 +36,7 @@ def _mark_notification_as_seen(notification_id: int):
 
 # Functions to send messages to a specific user
 #   - Each user is in a group named 'chat_{user_id}'
+#   ! can also send messages to ALL anonymous users -> when user_id == 'anonymous'
 def send_message_to_user(user_id, message):
     async_to_sync(channel_layer.group_send)(
         f"chat_{user_id}", {"type": "chat_message", "message": message}
@@ -76,13 +77,14 @@ class ChatConsumer(WebsocketConsumer):
         self.accept()
 
         # Banner for new chat
-        async_to_sync(self.channel_layer.group_send)(
-            self.room_group_name,
-            {
-                "type": "chat_message",
-                "message": f"YOU ARE CONNECTED TO {self.room_group_name}",
-            },
-        )
+        # FOR TESTING PURPOSES
+        # async_to_sync(self.channel_layer.group_send)(
+        #     self.room_group_name,
+        #     {
+        #         "type": "chat_message",
+        #         "message": f"YOU ARE CONNECTED TO {self.room_group_name}",
+        #     },
+        # )
 
         if self.user.is_authenticated:
             # Send all notifications that haven't been 'seen' in the database
